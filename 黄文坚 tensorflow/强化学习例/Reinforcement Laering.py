e719b332f187a31e8cd5a52cfdf1b9cdfa8a5a19
jinghaom <jinghaom@gmail.com>
2018-01-08 20:28:59 +0900

    报错了

--- 黄文坚 tensorflow/强化学习例/Reinforcement Laering.py
+++ 黄文坚 tensorflow/强化学习例/Reinforcement Laering.py
@@ -10,10 +10,10 @@ random_episodes = 0
 reward_sum = 0
 while random_episodes < 10:
     env.render()
-    observation, reward, done, _ = env.step(np.random.randint(0,2))
+    observation, reward, done, _ = env.step(np.random.randint(0, 2))
     reward_sum += reward
     if done:
         random_episodes += 1
-        print("reward fo this episod was:",reward_sum)
+        print("reward fo this episod was:", reward_sum)
         reward_sum = 0
         env.reset()
